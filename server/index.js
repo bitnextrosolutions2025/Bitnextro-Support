5f36bd3bb102cc6f7eee18c414fd1433bfd1a1de
skrijwan <skrijwan329@gmail.com>
2026-01-25 23:50:59 +0530

    update server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 connectDB();
+app.get("/",(req,res)=>{
+   return res.status(200).json({"message":"code run"})
+})
 app.use("/api/v1/auth",authRouter)
 app.listen(process.env.PORT,()=>{
     console.log(`your app is run in port:${process.env.PORT}`)
