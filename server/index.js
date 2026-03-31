27d1ee0274b955a182a6508a7f58a11fbd25d024
skrijwan <skrijwan329@gmail.com>
2026-03-31 15:19:51 +0530

    cores update

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import BlogRoute from "./routes/blog.js"
 const app = express();
 app.use(express.json());
 const coresoption = {
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: [
+        process.env.FRONTEND_URL,
+        process.env.FRONTEND_URL2
+    ],
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'PUT', 'PATCH'], // Ensure methods are in an array
     allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'],
     credentials: true, // Allow cookies/auth headers
@@ -18,14 +21,14 @@ const coresoption = {
 }
 app.use(cors(coresoption));
 await connectDB();
-app.get("/",(req,res)=>{
-   return res.status(200).json({"message":"code run"})
+app.get("/", (req, res) => {
+    return res.status(200).json({ "message": "code run" })
 })
-app.use("/api/v1/auth",authRouter);
-app.use("/api/v2/tickt",ticketRouter);
-app.use("/api/v3/bill",billingRoute);
-app.use("/api/v4/copybill",billingcopyRoute);
-app.use("/api/v5/blog",BlogRoute)
-app.listen(process.env.PORT,()=>{
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v2/tickt", ticketRouter);
+app.use("/api/v3/bill", billingRoute);
+app.use("/api/v4/copybill", billingcopyRoute);
+app.use("/api/v5/blog", BlogRoute)
+app.listen(process.env.PORT, () => {
     console.log(`your app is run in port:${process.env.PORT}`)
 });
